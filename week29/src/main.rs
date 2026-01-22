9de733606f2db03821a47c83fa3abc77a3a6c3fc
amaan-malik1 <amikm1077@gmail.com>
2026-01-22 06:53:07 +0000

    macros assignment done

--- week29/src/main.rs
+++ week29/src/main.rs
@@ -9,19 +9,55 @@
 //     Hello!();
 // }
 
-#[derive(Debug)]
-struct User {
-    username: String,
-    email: String,
-    age: u32,
+// #[derive(Debug)]
+// struct User {
+//     username: String,
+//     email: String,
+//     age: u32,
+// }
+
+// fn main() {
+//     let u = User {
+//         username: String::from("Amaan"),
+//         email: String::from("amikm@gmial.com"),
+//         age: 22,
+//     };
+
+//     println!("{:?}", u);
+// }
+
+// #[derive(Debug, Copy, Clone)]
+// struct User {
+//     username: String,
+//     age: u32,
+// }
+
+// fn main() {
+//     let u = User {
+//         username: String::from("Aman malik"),
+//         age: 22,
+//     };
+
+//     let u2 = u;
+
+//     println!("{:?}", u);
+//     println!("{:?}", u2);
+// }
+
+macro_rules! generate_mul_functions{
+   ($($func_name:ident),*)=>{
+    $(
+        fn $func_name(){
+            println!("heelo from {} fucntion..", stringify!($func_name));
+        }
+    )*
+   };
 }
 
-fn main() {
-    let u = User {
-        username: String::from("Amaan"),
-        email: String::from("amikm@gmial.com"),
-        age: 22,
-    };
+generate_mul_functions!(bro, saas, nand);
 
-    println!("{:?}", u);
+fn main() {
+    bro();
+    saas();
+    nand();
 }
